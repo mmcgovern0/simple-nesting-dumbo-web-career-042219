85f108a6386b3c671c4b2b9568bd2edd8d258715
Michael McGovern <mmcgovern.wework@gmail.com>
2019-04-14 18:24:11 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -21,9 +21,7 @@ def hopper
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
-	programmer_hash = 
+  programmer_hash = 
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -38,6 +36,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
